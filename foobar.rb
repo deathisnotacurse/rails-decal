a0ac2091a5fae0284decc54aa9aa9606c944763b
Yitian Zou <yitian.zou@berkeley.edu>
2016-02-12 15:22:55 -0800

    done

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(array)
+  	array = array.map! {|x| Integer(x)}
+  	array = array.map! {|x| x + 2}
+  	array = array.select{|x| x % 2 == 0}
+  	array = array.uniq
+  	array = array.select{|x| x <= 10}
+  	sum = 0
+  	array.each {|x| sum+=x}
+  	return sum
+  end
 end
